58bc27226e89e388adf9dc353ac822888b960a6a
unknown <andreitalopez250@gmail.com>
2018-05-21 22:38:12 -0500

    commit local

--- src/app/inventario/inventario.component.ts
+++ src/app/inventario/inventario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-inventario',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./inventario.component.css']
 })
 export class InventarioComponent implements OnInit {
+  formgroup: FormGroup
 
-  constructor() { }
+  private name = null
+  private amount = 1
+  private price = 1000
+
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formgroup = this.formbuilder.group({
+      name: [this.name, Validators.required],
+      amount: [this.amount, Validators.compose([Validators.required, Validators.min(0)])],
+      price: [this.price, Validators.compose([Validators.required, Validators.min(0)])]
+
+    })
   }
 
 }
--- src/app/my-dashboard/my-dashboard.component.spec.ts
+++ src/app/my-dashboard/my-dashboard.component.spec.ts
@@ -1,24 +1,24 @@
 
-import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { MyDashboardComponent } from './my-dashboard.component';
+import { MyDashboardComponent } from './my-dashboard.component'
 
 describe('MyDashboardComponent', () => {
-  let component: MyDashboardComponent;
-  let fixture: ComponentFixture<MyDashboardComponent>;
+  let component: MyDashboardComponent
+  let fixture: ComponentFixture<MyDashboardComponent>
 
   beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyDashboardComponent ]
+      declarations: [MyDashboardComponent]
     })
-    .compileComponents();
+      .compileComponents()
 
-    fixture = TestBed.createComponent(MyDashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+    fixture = TestBed.createComponent(MyDashboardComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  }))
 
   it('should compile', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
